72d38c7698c0a3f72c3294b035ac6607bab0bb32
Mattie Gonzalez <gonzalem@lssc.edu>
2023-03-30 10:41:01 -0400

    adding Navbar css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -21,27 +21,33 @@ export const NavBar = () => {
 
         return () => window.removeEventListener("scroll", onScroll);
     }, [])
+    
+    const onUpdateActiveLink = (value) => {
+        setActiveLink(value);
+    }
 
     return (
         <Navbar expand="lg" bg="dark" variant="dark" className = {scrolled ? "scrolled" : ""}>
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home" className = {activeLink === 'home' ? 'active navbar-link' : 'navbar-link'}>Home</Nav.Link>
-            <Nav.Link href="#bio" className = {activeLink === 'bio' ? 'active navbar-link' : 'navbar-link'}>Bio</Nav.Link>
-            <Nav.Link href="#skills" className = {activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'}>Features</Nav.Link>
-            <Nav.Link href="#projects"className = {activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'}>Pricing</Nav.Link>
-            <Nav.Link href="#resume" className = {activeLink === 'resume' ? 'active navbar-link' : 'navbar-link'}>Resume</Nav.Link>
-            <Nav.Link href="#contact" className = {activeLink === 'contact' ? 'active navbar-link' : 'navbar-link'}>Contact</Nav.Link>
+            <Nav.Link href="#home" className = {activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick = {() => onUpdateActiveLink('home')}>Home</Nav.Link>
+            <Nav.Link href="#bio" className = {activeLink === 'bio' ? 'active navbar-link' : 'navbar-link'} onClick = {() => onUpdateActiveLink('bio')}>Bio</Nav.Link>
+            <Nav.Link href="#skills" className = {activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick = {() => onUpdateActiveLink('skills')}>Features</Nav.Link>
+            <Nav.Link href="#projects"className = {activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick = {() => onUpdateActiveLink('projects')}>Pricing</Nav.Link>
+            <Nav.Link href="#resume" className = {activeLink === 'resume' ? 'active navbar-link' : 'navbar-link'} onClick = {() => onUpdateActiveLink('resume')}>Resume</Nav.Link>
+            <Nav.Link href="#contact" className = {activeLink === 'contact' ? 'active navbar-link' : 'navbar-link'} onClick = {() => onUpdateActiveLink('contact')}>Contact</Nav.Link>
           </Nav>
+          {/* add icons for socials linkedin github */}
           <span className="navbar-text">
             <div className="social-icon">
-                <a href="#"><img src={} alt=""/></a>
-                <a href="#"><img src={} alt=""/></a>
-                <a href="#"><img src={} alt=""/></a>
+                <a href="#"></a>
+                <a href="#"></a>
+                <a href="#"></a>
             </div>
             <button className="contactbtn" onClick={() => console.log ('connect')}><span>Submit</span></button>
           </span>
+          
         </Container>
       </Navbar>
     );
