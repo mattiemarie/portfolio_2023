8a46aa1d42630fadf35350d40dd752c8372b730d
Mattie-Marie <gonzalem@lssc.edu>
2023-04-26 13:54:54 -0400

    adding images to projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Card, CardBody, CardFooter, Text, Image, Stack, Heading } from '@chakra-ui/react'
+import picklefuzzImage from '../components/images/picklefuzz.jpg';
+
 
 function Projects() {
   return (
@@ -17,7 +19,7 @@ function Projects() {
       <Card className='card' maxW='sm'>
         <CardBody className='card-body'>
           <Image className='card-image'
-            src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+            src={picklefuzzImage}
             alt='Green double couch with wooden legs'
             borderRadius='lg'
           />
